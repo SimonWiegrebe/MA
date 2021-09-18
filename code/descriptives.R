e637d4489d710f884bab79ff75774bc992ef649b
simonwiegrebe1992 <simon.wiegrebe@gmail.com>
2021-09-18 12:06:58 +0200

    figures 3

--- code/descriptives.R
+++
@@ -1,3 +0,0 @@
-a <- readRDS("../data/sequences_all_anon.RDS")
-b <- readRDS("../data/sequences_all_anon_1.RDS")
-c <- readRDS("../data/sequences_all_anon_wild.RDS")
---
+++ code/old/descriptives.R
@@ -0,0 +1,3 @@
+a <- readRDS("../data/sequences_all_anon.RDS")
+b <- readRDS("../data/sequences_all_anon_1.RDS")
+c <- readRDS("../data/sequences_all_anon_wild.RDS")
